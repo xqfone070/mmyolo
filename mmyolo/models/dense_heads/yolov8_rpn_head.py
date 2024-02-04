96b034e1c71856267340e5834aa567a482f2af11
alex <xqfone070@gmail.com>
2024-02-04 09:38:27 +0800

    yolov8_rpn_head增加freeze_all功能

--- mmyolo/models/dense_heads/yolov8_rpn_head.py
+++ mmyolo/models/dense_heads/yolov8_rpn_head.py
@@ -5,6 +5,7 @@ from mmengine.structures import InstanceData
 from mmdet.utils import ConfigType
 from mmyolo.registry import MODELS
 from .yolov8_head import YOLOv8Head
+from torch.nn.modules.batchnorm import _BatchNorm
 
 
 @MODELS.register_module()
@@ -13,8 +14,10 @@ class YOLOv8RPNHead(YOLOv8Head):
     def __init__(self,
                  head_module: ConfigType,
                  num_classes: int = 1,
+                 freeze_all: bool = False,
                  **kwargs):
         head_module.update(num_classes=num_classes)
+        self.freeze_all = freeze_all
         super(YOLOv8RPNHead, self).__init__(head_module, **kwargs)
 
     # 作为rpn头时，传入的objectnesses固定为None
@@ -34,3 +37,18 @@ class YOLOv8RPNHead(YOLOv8Head):
                                        cfg=cfg,
                                        rescale=rescale,
                                        with_nms=with_nms)
+
+    def _freeze_all(self):
+        """Freeze the model."""
+        for m in self.modules():
+            if isinstance(m, _BatchNorm):
+                m.eval()
+            for param in m.parameters():
+                param.requires_grad = False
+
+    def train(self, mode=True):
+        """Convert the model into training mode while keep the normalization
+        layer freezed."""
+        super().train(mode)
+        if self.freeze_all:
+            self._freeze_all()
